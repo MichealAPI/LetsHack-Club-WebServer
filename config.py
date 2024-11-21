86887bf62756f3017a39c16ef6c49d0d7e4149c9
Michele <michele.cippone01@gmail.com>
2024-11-21 17:42:00 +0100

    Fixing minor security issues with some previously exposed local-testing credentials

--- config.py
+++ config.py
@@ -2,5 +2,4 @@ import os
 
 
 class Config:
-    MONGO_URI = "mongodb+srv://test_unit:GjK3oux4z7laRaQ0CGhL7VvjikpIicx8L7co4ljxiYg1k4bAlyayzX2teiFXkrw40R8QGOdgaekCAuo6bunoUhilc6W0fe6dVd2p73JTLUVS8omaHnHvbxNoJf1zw2aQ@mikeslab-development.puqr1c4.mongodb.net/letshack"  #(os.environ.get('MONGO_URI') or
-                 #'mongodb://localhost:27017/letshack')
+    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/letshack'
